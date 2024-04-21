a3ae062f21bf400a5298c556546287fccced4efc
Chenghao Mou <mouchenghao@gmail.com>
2024-04-21 11:08:27 +0100

    weekly sync

--- scripts/created.py
+++ scripts/created.py
@@ -3,17 +3,19 @@ import glob
 import frontmatter
 from pathlib import Path
 
-for file in glob.glob("./notes/*.md"):
-    basename = os.path.basename(file)
-    if not basename.startswith("202"):
-        continue
-    # update the front matter for the crated date
-    timestamp = basename.split(".", 1)[0]
-    year, month, day = timestamp[:4], timestamp[4:6], timestamp[6:8]
-    post = frontmatter.loads(Path(file).read_text())
-    if "created" in post:
-        continue
-    post["created"] = f'{year}-{month}-{day}'
-    # print(frontmatter.dumps(post))
-    with open(file, "w") as f:
-        f.write(frontmatter.dumps(post))
+if __name__ == "__main__":
+    print("Updating dates")
+    # for file in glob.glob("./notes/*.md"):
+    #     basename = os.path.basename(file)
+    #     if not basename.startswith("202"):
+    #         continue
+    #     # update the front matter for the crated date
+    #     timestamp = basename.split(".", 1)[0]
+    #     year, month, day = timestamp[:4], timestamp[4:6], timestamp[6:8]
+    #     post = frontmatter.loads(Path(file).read_text())
+    #     post["created"] = f'{year}-{month}-{day}'
+    #     post["date"] = f'{year}-{month}-{day}'
+    #     post["modified"] = f'{year}-{month}-{day}'
+    #     # print(frontmatter.dumps(post))
+    #     with open(file, "w") as f:
+    #         f.write(frontmatter.dumps(post))
